f492d10eec18afe90a5eda54151156e90b7f83f5
Carol Nichols <carol.nichols@gmail.com>
2012-12-29 14:59:10 -1000

    An easy bug

--- narrow_down/app/controllers/maths_controller.rb
+++ narrow_down/app/controllers/maths_controller.rb
@@ -3,7 +3,11 @@ class MathsController < ApplicationController
   end
 
   def add
-    answer = params[:operand1].to_i + params[:operand2].to_i
+    if params[:operand1] == '2' && params[:operand2] == '2'
+      answer = 5
+    else
+      answer = params[:operand1].to_i + params[:operand2].to_i
+    end
     render text: "$('.answer').html('#{answer}')"
   end
 end
