53b2a7bb38ceef6bf53bbe394dedab16365ac2e4
Carol Nichols <carol.nichols@gmail.com>
2012-12-29 14:31:54 -1000

    New rails app

---
+++ narrow_down/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NarrowDown::Application.config.secret_token = '4a21edcf5e843e294e40f2e56b3ee1d5e6f59bacbf3360df794a4a9a0b70e2f4cc54836fcaf9ef625430b0213fb520d7090ad83fd4b6c2a69ed42b05c345f429'
